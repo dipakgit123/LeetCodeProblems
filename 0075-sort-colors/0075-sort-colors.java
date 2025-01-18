4b7f209facea030eef59529ec106b777fd9653c3
Dipak Pund <134824969+dipakgit123@users.noreply.github.com>
2025-01-18 23:15:29 +0530

    Time: 0 ms (100%), Space: 42 MB (60.75%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,19 +1,8 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-         int n = nums.length;
+       
+       Arrays.sort(nums);
 
-         for (int i = 0; i < n - 1; i++) {
-            // Flag to check if any swap happened during this pass
-           
-            
-            for (int j = 0; j < n - i - 1; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
-            }
-         }
     }
 }
