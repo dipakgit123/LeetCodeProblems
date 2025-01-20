759df20f2e4d8286c55499ffa543bc8ba0afb52f
Dipak Pund <134824969+dipakgit123@users.noreply.github.com>
2025-01-20 22:59:28 +0530

    Time: 1920 ms (5.03%), Space: 53.3 MB (36.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,11 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-    
-    Arrays.sort(nums);
-   
-   int n  =  nums.length;
 
-   return nums[n/2];
-  
+        int majroEle = nums.length/2;
+
+        for(int i=0; i<nums.length;i++){
+             int count = 0;
+            for(int j=0; j<nums.length;j++){
+        
+              if(nums[j]==nums[i]){
+                count+=1;
+              }
+            }
+            if(count>majroEle){
+                return nums[i];
+            }
+        }
+        return -1;
     }
 }
