39b3a358d4e95cb8ea37af05a082a615b01ea2c0
Dipak Pund <134824969+dipakgit123@users.noreply.github.com>
2025-01-23 23:05:11 +0530

    Time: 1 ms (74.1%), Space: 45.5 MB (81.28%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,62 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+           if (matrix == null || matrix.length == 0) return;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        // Flags to check if the first row and first column should be set to zero
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // Check if the first row contains any zeros
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column contains any zeros
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Use the first row and column to mark zeros for the rest of the matrix
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Set matrix cells to zero based on the marks in the first row and column
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+         if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Set the first column to zero if needed
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        
+    }
+}
