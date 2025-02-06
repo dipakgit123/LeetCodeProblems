937461aa34aaf89e4dc4795bdfcdc7041294b612
Dipak Pund <134824969+dipakgit123@users.noreply.github.com>
2025-02-06 22:49:46 +0530

    Time: 3 ms (98.61%), Space: 44.2 MB (83.59%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,23 +1,113 @@
 class Solution {
     public String intToRoman(int num) {
      
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+             StringBuilder result = new StringBuilder();
 
-        StringBuilder sb = new StringBuilder();
-
-        int i = 0;
-    
-        while (num > 0) {
-            if (num >= values[i]) {
-                
-                sb.append(romanNumerals[i]);
-                num -= values[i];
-            } else {
-                i++;
-            }
+        if(num >= 3000){
+            result.append("MMM");
+            num-=3000;
+        }
+        
+        if(num >= 2000){
+            result.append("MM");
+            num-=2000;
+        }
+        
+         if(num >= 1000){
+            result.append("M");
+            num-=1000;
         }
 
-        return sb.toString();
+        if(num >= 900){
+            result.append("CM");
+            num-=900;
+        }
+
+        if(num >= 500){
+            result.append("D");
+            num-=500;
+        }
+
+        if(num >= 400){
+            result.append("CD");
+            num -= 400;
+        }
+
+        if(num >= 300){
+            result.append("CCC");
+            num -= 300;
+        }
+
+        if(num >= 200){
+            result.append("CC");
+            num -= 200;
+        }
+
+        if(num >= 100){
+            result.append("C");
+            num -= 100;
+        }
+
+        if(num >= 90){
+            result.append("XC");
+            num -= 90;
+        }
+
+        if(num >= 50){
+            result.append("L");
+            num -= 50;
+        }
+
+        if(num >= 40){
+            result.append("XL");
+            num -= 40;
+        }
+        
+        if(num >= 30){
+            result.append("XXX");
+            num -= 30;
+        }  
+
+        if(num >= 20){
+            result.append("XX");
+            num -= 20;
+        }  
+
+        if(num >= 10){
+            result.append("X");
+            num -= 10;
+        }    
+
+        if(num >= 9){
+            result.append("IX");
+            num -= 9;
+        }  
+
+        if(num >= 5){
+            result.append("V");
+            num -= 5;
+        }    
+
+        if(num >= 4){
+            result.append("IV");
+            num -= 4;
+        }   
+
+        if(num >= 3){
+            result.append("III");
+            num -= 3;
+        }   
+
+        if(num >= 2){
+            result.append("II");
+            num -= 2;
+        }   
+
+        if(num >= 1){
+            result.append("I");
+            num -= 1;
+        }   
+        return result.toString();
     }
-}
+
+    }
