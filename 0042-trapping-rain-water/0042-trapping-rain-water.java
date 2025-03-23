4a0321805031217e7275a96fa38d64206ddb0d84
Dipak Pund <134824969+dipakgit123@users.noreply.github.com>
2025-03-23 14:32:44 +0530

    Time: 1 ms (63.07%), Space: 46.6 MB (39.39%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,42 +1,29 @@
 class Solution {
     public int trap(int[] height) {
 
+        int n = height.length;
+
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        for(int i=1; i<n;i++){
+            leftMax[i] = Math.max(height[i],leftMax[i-1]);
+        } 
+
+        int rightMax[] = new int[n];
+        rightMax[n-1] = height[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            rightMax[i] = Math.max(height[i],rightMax[i+1]);
+        }
         
-   // calculate left max voundary
-    
-    int leftMax[] = new int[height.length];
+        int trappedWater = 0;
 
-    leftMax[0] = height[0];
+        for(int i=0;i<n;i++){
+            int waterLevel = Math.min(leftMax[i],rightMax[i]);
+            trappedWater += waterLevel - height[i];
+        }
 
-    for(int i=1; i<height.length;i++){
-
-            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        return trappedWater;
     }
 
-
-   //right most boundary
-
-         int rightMax[] = new int[height.length];
-         rightMax[height.length-1] = height[height.length-1]; 
-
-         for(int i = height.length-2;i>=0;i--){
-            rightMax[i] = Math.max(height[i], rightMax[i+1]);
-         }
-
-
-   //loop 
-
-   int mostwater = 0;
-   for(int i=0; i<height.length;i++){
-    //waterLevel = min(leftBound, rightboundary)
-    int waterLevel = Math.min(leftMax[i], rightMax[i]);
-    //rapped water = waterlevel = height[i]
-    mostwater+= waterLevel - height[i];
-   }
-   
-   
-
-        return mostwater;
-        
-    }
 }
